0eead8bf9d5d3c8cc88c3bc036c483d362299a0d
yulia-bisyuk <yulia.bisyuk@gmail.com>
2023-08-14 15:24:50 +0300

    feat: push-gh-pages script added

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 'use strict';
-import {
-  createNote,
-  deleteNote,
-  editNote,
-  setArchived,
-} from './src/js/actions';
-// import './src/js/refs';
-// import './src/js/actions';
-import {
-  populateMainTable,
-  populateSummaryTable,
-} from './src/js/populateTables';
-// import './src/js/populateTables';
+
+import './src/js/actions';
+import './src/js/populateTables';
 import './src/js/toggleModal';
